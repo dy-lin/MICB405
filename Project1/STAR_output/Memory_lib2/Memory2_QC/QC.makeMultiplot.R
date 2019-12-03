3f77a07152dfb6dee3e93973bd79b056c8701fab
Diana Lin <dlin@bcgsc.ca>
2019-12-03 08:15:07 +0000

    Add STAR files

---
+++ Project1/STAR_output/Memory_lib2/Memory2_QC/QC.makeMultiplot.R
@@ -0,0 +1,18 @@
+# This is an automatically-generated R script designed to make a simple multiplot and/or pdf report for a sample.
+message("STARTING...");
+library(QoRTs);
+unique.ID <- c("Untitled");
+qc.data.dir <- c("/projects/micb405/project1/Team10/STAR_output/Memory_lib2//Memory2_QC//");
+qc.data.prefix <- c("");
+decoder.raw <- data.frame(unique.ID = as.character(unique.ID), qc.data.dir = as.character(qc.data.dir), qc.data.prefix=as.character(qc.data.prefix),stringsAsFactors=FALSE);
+decoder <- completeAndCheckDecoder(decoder = decoder.raw)
+message(decoder);
+message(lapply(names(decoder), function(n){ class(decoder[[n]]) }));
+res <- read.qc.results.data("", decoder = decoder, calc.DESeq2 = FALSE, calc.edgeR = FALSE);
+#makeMultiPlot.basic(res, outfile = "/projects/micb405/project1/Team10/STAR_output/Memory_lib2//Memory2_QC//QC.multiPlot.png", plotter.params = list(std.color = "blue", std.lines.lwd = 4), plot.device.name = "png");
+makeMultiPlot.basic(res, outfile = "/projects/micb405/project1/Team10/STAR_output/Memory_lib2//Memory2_QC//Memory2_QC.multiPlot.pdf", plotter.params = list(std.color = "blue", std.lines.lwd = 4), plot.device.name = "pdf");
+#makeMultiPlot.basic(res, outfile.dir = paste0("/projects/micb405/project1/Team10/STAR_output/Memory_lib2//Memory2_QC/","QCplots/"), plotter.params = list(std.color = "blue", std.lines.lwd = 4), plot.device.name = "png", separatePlots = TRUE);
+message("DONE...");
+
+
+
---
+++ Project1/STAR_output/README.md
@@ -0,0 +1,11 @@
+# Files
+
+## Files Too Big
+* `Memory_lib2/Memory_2Aligned.sortedByCoord.out.bam`
+* `Memory_lib3/Memory_3Aligned.sortedByCoord.out.bam`
+* `Naive_lib1/Naive_1Aligned.sortedByCoord.out.bam`
+* `Naive_lib2/Naive_2Aligned.sortedByCoord.out.bam`
+* `Naive_lib3/Naive_3Aligned.sortedByCoord.out.bam`
+* `Primary_lib1/Primary_1Aligned.sortedByCoord.out.bam`
+* `Primary_lib2/Primary_2Aligned.sortedByCoord.out.bam`
+* `Primary_lib3/Primary_3Aligned.sortedByCoord.out.bam`
